a0f3c825497622c7c9c9211599deb18654b93b31
Aliisace <43137265+Aliisace@users.noreply.github.com>
2018-11-20 18:37:59 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Dependencies for OS
+apt-get install --assume-yes python3.7
+apt-get install --assume-yes --fix-missing python3-pip fping arp-scan nmap
+
+# Dependencies for Python(3.7+)
+python3 -m pip install yattag python-nmap 2to3
+python3.7 -m pip install yattag python-nmap 2to3
