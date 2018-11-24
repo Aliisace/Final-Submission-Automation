ad3d6c81dd1c675565a10198e435f7520cf36da3
Aliisace <43137265+Aliisace@users.noreply.github.com>
2018-11-24 12:29:41 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 # Dependencies for OS
-apt-get install --assume-yes python3.7
-apt-get install --assume-yes --fix-missing python3-pip fping arp-scan nmap
+apt-get install --assume-yes python2.7
+apt-get install --assume-yes python-pip fping arp-scan nmap
+sudo apt-get install vim
+sudo apt-get install git
+mkdir /home/el/.vim
+git clone https://github.com/sentientmachine/Pretty-Vim-Python.git
+mv Pretty-Vim-Python/* .
 
-# Dependencies for Python(3.7+)
-python3 -m pip install yattag python-nmap 2to3
-python3.7 -m pip install yattag python-nmap 2to3
+# Dependencies for Python(2.7+)
+pip install yattag python-nmap
--- script.py
+++ script.py
@@ -1,182 +1,122 @@
 import os
-import json
 import subprocess
 import sys
-import nmap
-import pickle
-import nmapParse
+from collections import namedtuple
 
+#Get users
+#Store users in user array
+#Get user info and store in array
+
+#print(['192.168.0.1']['users']['D.jin']['name'])
+#print(['192.168.0.1']['users']['D.jin']['rid'])
+#print(['192.168.0.1']['users']['D.jin']['groups'])
+#print(['192.168.0.1']['users']['D.jin']['groups']['rid'])
+#print(['192.168.0.1']['users']['D.jin']['groups']['']  )
+#print(['192.168.0.1']['users']['D.jin']['info'])
+
+#print(['192.168.0.1']['groups']['Administrator']['rid'])
+
+results = {}    
+ 
 # rpcclient credentials
 username = "test"
 password = "test123"
+ 
+def splitIntoDict(txt):
+    d = {} # dictionary of results
 
-hosts = {}
+    for index, i in enumerate(txt):
+        # split name and rid into an array
+        txt[index] = [txt[index][:txt[index].find("rid:")-1], txt[index][txt[index].find("rid:"):]]
+        # Splice out values and store in a dictionary
+        d[txt[index][0][txt[index][0].find(":[")+2:-1]] = {"name": txt[index][0][txt[index][0].find(":[")+2:-1], "rid": txt[index][1][5:-1]}
+    
+    # return the dictionary
+    return d
+# End splitIntoDicts
+ 
+def enumAliases(host):
+    command = ["rpcclient", host, "-U", username + "%" + password, "-c"]
+    dicts = {"builtin": {}, "domain":{}}
+ 
+    for i in dicts:
+        # Enumerate aliases and rids
+        dicts[i] = splitIntoDict(subprocess.run(command + ["enumalsgroups " + i], encoding = "ascii", stdout = subprocess.PIPE).stdout.splitlines())
+   
+    return dicts
+# End enumAliases
+ 
+def enumAliasMembers(host, users):
+    command = ["rpcclient", host, "-U", username + "%" + password, "-c"]
+    aliases = enumAliases(host) # call enumAliases
+    usrAliases = {} #dict of user Aliases
+    
+    for group in aliases:
+        for name in users:
+            users[name]["aliases"] = {group: {}}
+            # Get a list of domain aliases for said user
+            usrAliases[group] = subprocess.run(command + ["queryuseraliases " + group + " " + users[name]["sid"]], encoding = "ascii", stdout = subprocess.PIPE).stdout
+           
+            for alias in aliases[group]:
+                if aliases[group][alias]["rid"] in usrAliases[group]: 
+                    # Append alias to users[name]["aliases"]
+                    users[name]["aliases"][group][alias] = aliases[group][alias]
 
-def main():
-	# mode = getMode()
-	mode = "Lab"
-	if mode == "Test":
-		filename = "test.txt"
-		getIPs(filename)
-	elif mode == "Lab":
-		filename = "hostList.txt"
-		getIPs(filename)
-	getPing()
-	for ip in hosts:
-		searchMap(ip, "Unreachable")
-	print((json.dumps(hosts, sort_keys = True)))
-	for ip in hosts:
-		if ping(ip):
-			if not os.path.exists(ip):
-				print(("mkdir " + ip))
-				os.system("mkdir " + ip)
-				scan(ip)
-				rpcEnumDomUsers(ip)
-				rpcEnumGroups(ip)
-				getUserGroupNames(ip)
-			else:
-				scan(ip)
-				rpcEnumDomUsers(ip)
-				rpcEnumGroups(ip)
-				getUserGroupNames(ip)
+    return users
 
+def enumGroups(host):
+    command = ["rpcclient", host, "-U", username + "%" + password, "-c"]
+    
+    # Enumerate groups and rids
+    groupsDict = splitIntoDict(subprocess.run(command+["enumdomgroups"], encoding="ascii", stdout=subprocess.PIPE).stdout.splitlines())
 
-def getIPs(fileName):
-	with open(fileName) as f:
-		for line in f:
-			(key, val) = line.split()
-	hosts[key] = val
-# end getIPs
+    return groupsDict
 
-def getPing():
-	for ip in hosts:
-		val = []
-		os.system("ping -c 1 " + ip + " > output.txt")
-		with open("output.txt") as f:
-			for line in f:
- 				val.append(line)
-		hosts[ip] = val[1]
-# end getPing
+def enumGroupMembers(host, users):
+    command = ["rpcclient", host, "-U", username + "%" + password, + "-c"]
+    groups = enumGroups(host) # Call enumGroups
 
-def searchMap(searchKey, searchVal):
- 	for ip in hosts:
- 		if (searchKey in hosts):
- 			if (searchVal not in hosts[searchKey]):
- 				return "Host OK"
- 			hosts[searchKey] = "Host not available"
- 			return hosts[searchKey]
-# end searchMap
+    for name in users:
+        users[name]["groups"] = {} # set users[name]["groups"] equal to an empty dict
+        # get a list of groups for that user
+        usrGroups = subprocess.run(command + ["queryusergroups " + users[name]["rid"]], encoding = "ascii", stdout = subprocess.PIPE).stdout
 
-def ping(host):
-	os.system("fping %s > %s.txt" % (host, host))
-	pingFile = open("%s.txt" % host, 'r')
-	# os.system("rm %s.txt" % host)
+        for group in groups:
+            if groups[group]["rid"] in usrGroups:
+                # Append group to users[name]["aliases"]
+                users[name]["groups"][group] = groups[group]
 
-	if 'alive' in pingFile.read():
-		print("%s Alive on ICMP" % host)
-		return True
-	else:
-		print("%s Unavailable via ICMP" % host)
-		return False
-# end ping
+    return users
 
-# Runs an nmap scan on the host and parses using nmapParse.py
-def scan(host):
-	print("Scanning " + host)
-	scanner = nmap.PortScanner()
-	scanner.scan(host, arguments="-sV -O --script vuln")
-	nmapParse.scanParse(scanner[host], host + "/nmap.txt")
-# end scan
+def enumUsers(host):
+    command = ["rpcclient", host, "-U", username + "%" + password, + "-c"]
 
-# Uses rpcclient to enumerate for dom users
-# Writes list of users to host/users.txt
-# Runs rpcQueryUser for each user found
-def rpcEnumDomUsers(host):
-	os.system("rpcclient " + host + " -U " + username + "%" + password + " -c enumdomusers > " + host + "/users.txt")
-	userFile = open(host + "/users.txt")
-	users = []
-	for line in userFile:
-		users.append(line[6:-14])
-		os.system("mkdir " + host + "/users")
-	for user in users:
-		rpcQueryUser(host, user)
-# end rpcEnumDomUsers
+    # Enumerate usernames and rids
+    usersTxt = subprocess.run(command + ["queryusergroups " + users[name]["rid"]], encoding = "ascii", stdout = subprocess.PIPE).stdout
+    
+    for name in usersDict:
+        # Enumerate SID and info of each user
 
-# Uses rpcclient to enumerate info about individual users
-# Writes the info to host/users/user.txt
-def rpcQueryUser(host, user):
-	os.system("rpcclient " + host + " -U " + username + "%" + password + " -c \'queryuser \"" + user + "\"\' > " + host + "/users/\'" + user + ".txt\'")
-# end rcpQueryUser
+        usersDict[name]["rawinfo"] = subprocess.run(command + ["queryuser " + usersDict[name]["rid"]], encoding = "ascii", stdout = subprocess.PIPE).stdout
+        usersDict[name]["sid"] = subprocess.run(command + ["lookupnames " + name], encoding = "ascii", stdout = subprocess.PIPE).stdout
+        usersDict[name]["sid"] = usersDict[name]["sid"][usersDict[name]["sid"].find("S-"):usersDict[name]["sid"].find(" (User")]
 
-# Uses rpcclient to enumerate for all group types
-# Writes the groups to host/groups.txt
-def rpcEnumGroups(host):
-	commands = ['enumdomgroups', '\'enumalsgroups builtin\'', '\'enumalsgroups domain\'']
-	for i in commands:
-		os.system("rpcclient " + host + " -U " + username + "%" + password + " -c " + i + " >> " + host + "/groups.txt")
-# end rpcEnumGroups
+    usersDict = enumGroupMembers(host, usersDict) # call enumGroupMembers
+    usersDict = enumAliasMembers(host, usersDict) # call enumAliasMembers
 
-# Sorts user txt files into folders of their respective groups
-def getUserGroupNames(host):
-	occupiedGroups = []
-	userGroupId = {}
-	usersDir = os.listdir(host + "/users")
-	groups = open(host + "/groups.txt", 'r')
-
-	# Populates userGroupId dict with the users and their group ids
-	for user in usersDir:
-		tempUserFile = open(host + "/users/" + user)
-		for i, line in enumerate(tempUserFile):
-		    if i == 0:
-		        # Slice name from line 0
-		        nm = line[15:-1]
-		    if i == 18:
-		        # Slice group id from line 18
-		        gid = line[12:-1]
-		userGroupId[nm] = gid
-
-	# Creates a directory to sort the users into
-	os.system("mkdir " + host + "/sortedUsers")
-
-	# For each group
-	for group in groups:
-		# Make a folder of that groups name
-		os.system("mkdir " + host + "/sortedUsers/\'" + group[7:-14] + "\'")
-		# For each user
-		for user in userGroupId:
-		# Check if user is in the current group
-			if userGroupId[user] == group[-7:-2]:
-				# If so, move their .txt file into that group folder
-				os.system("mv " + host + "/users/\'" + user + ".txt\' " + host + "/sortedUsers/\'" + group[7:-14] + "\'/")
-				# Add group to the list of occupied groups
-				if group[7:-14] not in occupiedGroups:
-					occupiedGroups.append(group[7:-14])
-
-	# For all occupied groups
-	for group in occupiedGroups:
-		# Move the groups folder to host/users
-		os.system("mv " + host + "/sortedUsers/\'" + group + "\'" + " " + host + "/users")
-	# Remove unoccupied folders
-	os.system("rm -r " + host + "/sortedUsers")
-# end getUserGroupNames
-
-
-def getMode():
-	while True:
-		print(("\t" + os.path.basename(__file__)))
-		mode = input("mode > ").upper()
-		if mode in "T L Q".split():
-			if mode[0] == "T":
-				mode = "Test"
-				return mode
-			elif mode[0] == "L":
-				mode = "Lab"
-				return mode
-			elif mode[0] == "Q":
-				sys.exit()
-		else:
-			print("If you are testing please press T\n If you are in the lab please press L")
-
+    return usersDict
 
 if __name__ == "__main__":
-	main()
+    hosts = sys.argv[1:]
+    print(os.path.basename(__file__))
+    for host in hosts:
+        print(host + "\n\n")
+        results[host] = {"users": enumUsers(host)}
+        for host in hosts:
+            print(host + ":\n\n")
+            results[host] = {"users": enumUsers(host)}
+            for index,user in enumerate(results[host]["users"]):    
+                print("User: " + user + "\t", end="")
+                if len(user) < 10:
+                    print("\t", end="")
+                print("| SID: " + results[host]["users"][user]["sid"])
